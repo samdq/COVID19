364a8e4f3f2c8db4bd6811734ed14b801d5900ad
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2020-09-25 21:47:21 +0530

    update file

--- models.py
+++ models.py
@@ -6,3 +6,10 @@ class HospitalBed:
         self.city = city
         self.pincode = pincode
         self.available_beds = available_beds
+
+class Oxygen:
+    def __init__(self, state, city, pincode, available_oxygen):
+        self.state = state
+        self.city = city
+        self.pincode = pincode
+        self.available_oxygen = available_oxygen
