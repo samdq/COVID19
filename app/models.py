4bdfa14399b68aebd89e4e13f317763c20ee371d
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2023-07-01 15:35:41 +0530

    update file

--- app/models.py
+++ app/models.py
@@ -13,10 +13,3 @@ class Oxygen:
         self.city = city
         self.pincode = pincode
         self.available_oxygen = available_oxygen
-        
-class Medicine:
-    def __init__(self, state, city, pincode, available_medicines):
-        self.state = state
-        self.city = city
-        self.pincode = pincode
-        self.available_medicines = available_medicines
