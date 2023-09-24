e568bdd3b7370d86b3807fb26dc15a56dfd5b139
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2023-09-24 15:34:37 +0530

    update file

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,15 @@ from app.services import (
     update_feedback
 )
 
+@app.route('/hospital-beds', methods=['GET'])
+def hospital_beds():
+    state = request.args.get('state')
+    city = request.args.get('city')
+    pincode = request.args.get('pincode')
 
+    data = get_hospital_beds(state, city, pincode)
+
+    return jsonify(data)
 
 @app.route('/oxygen', methods=['GET'])
 def oxygen():
